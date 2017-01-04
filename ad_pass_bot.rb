009c99180477a95e21d230078fecc2ed5868524c
Jeremy Shapiro <jeremynshapiro@gmail.com>
2017-01-03 18:02:50 -0800

    dockerize; add range of days to notify; fix date

--- ad_pass_bot.rb
+++ ad_pass_bot.rb
@@ -14,14 +14,14 @@ ous_to_check = [ENV['OU_1'], ENV['OU_2'], ENV['OU_3']]
 ## AD
 # Initialize connection to AD
 ldap = Net::LDAP.new :host => ENV['AD_HOST'],
-    :port => 636,
-    :base => ENV['AD_BASE'],
-    :encryption => :simple_tls,
-    :auth => {
-      :method => :simple,
-      :username => ENV['AD_USERNAME'],
-      :password => ENV['AD_PASSWORD']
-    }
+  :port => 636,
+  :base => ENV['AD_BASE'],
+  :encryption => :simple_tls,
+  :auth => {
+    :method => :simple,
+    :username => ENV['AD_USERNAME'],
+    :password => ENV['AD_PASSWORD']
+  }
 
 # Find all users and relevant attributes
 ous_to_check.each do |ou|
@@ -30,36 +30,46 @@ ous_to_check.each do |ou|
 
   ldap.search(:base => treebase, :filter => filter) do |entry|
     attributes = {}
-    attributes[:mail] = entry.mail.join
-    attributes[:PwdExpireTime] = Time.at(entry.PwdLastSet.join.to_i) + 90.days
+    attributes[:mail] = entry.mail.join.downcase
+    attributes[:PwdExpireTime] = Time.at(((entry.PwdLastSet.join.to_i)/10000000) - 11644473600) + 90.days
     users[entry.name.join] = attributes
-    puts attributes
   end
 end
 
+# Output All users for logging
+puts "Found users: #{users}"
+
 # Determine users with passwords about to expire
 users.each do |user, attr|
-  if Time.now >= (attr[:PwdExpireTime] - 5.days)
-    users_to_notify[attr[:mail]] = {:PwdExpireTime => attr[:PwdExpireTime]}
-  else 
-    puts "Will not notify #{user}, password expires in greater than 5 days: #{attr[:PwdExpireTime]}"
+  if (-5..1).include?((Time.now - attr[:PwdExpireTime])/86400)
+    users_to_notify[attr[:mail]] = {:PwdExpireTime => attr[:PwdExpireTime].strftime("%A, %d %b %Y %l:%M %p")}
+  else
+    puts "Will not notify #{user}, password expires is greater than 5 days: #{attr[:PwdExpireTime]}"
   end
 end
 
 ## Slack
 # Initialize Slack Connection
-Slack.configure do |config|
-  config.token = ENV['SLACK_API_TOKEN']
-end
-client = Slack::Web::Client.new
-# Find Slack usernames and add to hash
-slack_users = client.users_list.to_hash
-slack_users["members"].each do |user|
-  if users_to_notify.include?(user["profile"]["email"])
-    users_to_notify[user["profile"]["email"]][:slack_name] = user["name"]
+if !users_to_notify.empty?
+  Slack.configure do |config|
+    config.token = ENV['SLACK_API_TOKEN']
   end
-end
-# Notify users
-users_to_notify.each do |user, attr|
-  client.chat_postMessage(channel: "@#{attr[:slack_name]}", text: "Your password will expire in less than 5 days at: #{attr[:PwdExpireTime]}. Reset your password by going to: #{ENV['PW_REST_URL']}", as_user: true)
+  client = Slack::Web::Client.new
+  # Find Slack usernames and add to hash
+  slack_users = client.users_list.to_hash
+  slack_users["members"].each do |user|
+    if users_to_notify.include?(user["profile"]["email"])
+      users_to_notify[user["profile"]["email"]][:slack_name] = user["name"]
+    end
+  end
+  # Notify users and output user for logging
+  users_to_notify.each do |user, attr|
+    if !attr[:slack_name].nil?
+      client.chat_postMessage(channel: "@#{attr[:slack_name]}", text: "Your Active Directory [e-mail] password will expire in less than 5 days at: #{attr[:PwdExpireTime]}. Reset your password by going to: #{ENV['PW_RESET_URL']}", as_user: true)
+      puts "Notified #{user} with slack name: #{attr[:slack_name]} that their password will expire at #{attr[:PwdExpireTime]}"
+    else
+      client.chat_postMessage(channel: "@#{ENV['BACKUP_SLACK_USER']}", text: "The AD password for #{user} will expire in less than 5 days at: #{attr[:PwdExpireTime]}. They do not have a slack account. Please notify them to reset their password by going to: #{ENV['PW_RESET_URL']}", as_user: true)
+      puts "#{user} does not have a slack name, Sent a message to #{ENV['BACKUP_SLACK_USER']}"
+    end
+  end
 end
