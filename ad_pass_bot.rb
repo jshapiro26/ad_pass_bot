38281988bdf9007486f77e46f00a93cdcf466655
Jeremy Shapiro <jeremynshapiro@gmail.com>
2017-03-21 14:20:23 -0700

    Add notifications to managers of expiring AD user accounts

--- ad_pass_bot.rb
+++ ad_pass_bot.rb
@@ -9,6 +9,7 @@ Dotenv.load
 # Initialize hashes and arrays for later
 users = {}
 users_to_notify = {}
+accounts_expiring = {}
 ous_to_check = [ENV['OU_1'], ENV['OU_2'], ENV['OU_3']]
 
 # How many days from password change does a password expire?
@@ -26,6 +27,11 @@ ldap = Net::LDAP.new :host => ENV['AD_HOST'],
     :password => ENV['AD_PASSWORD']
   }
 
+# Convert Windows NT Time to Unix epoch time
+def convert_time(int)
+  Time.at(((int)/10000000) - 11644473600)
+end
+
 # Find all users and relevant attributes
 ous_to_check.each do |ou|
   filter = Net::LDAP::Filter.eq("ObjectClass", "user")
@@ -33,9 +39,17 @@ ous_to_check.each do |ou|
 
   ldap.search(:base => treebase, :filter => filter) do |entry|
     attributes = {}
+    if entry.dn.include?(ENV['ACCT_EXP_GROUP'])
+      if entry.respond_to?(:manager)
+        attributes[:accountExpiresAt] = convert_time(entry.accountexpires.join.to_i)
+        attributes[:manager] = entry.manager.join
+      else
+        attributes[:accountExpiresAt] = convert_time(entry.accountexpires.join.to_i)
+        attributes[:manager] = "No Manager Listed"
+      end
+    end
     attributes[:mail] = entry.mail.join.downcase
-    # Convert Windows NT Time to UNIX epoch time and add number of days until expiry to find expire time
-    attributes[:PwdExpireTime] = Time.at(((entry.PwdLastSet.join.to_i)/10000000) - 11644473600) + expire_after.days
+    attributes[:PwdExpireTime] = convert_time(entry.PwdLastSet.join.to_i) + expire_after.days
     users[entry.name.join] = attributes
   end
 end
@@ -48,20 +62,35 @@ users.each do |user, attr|
   # Only include users with passwords that will expire in 5 days and up to 1 day past expiry
   if (-5..1).include?((Time.now - attr[:PwdExpireTime])/86400)
     users_to_notify[attr[:mail]] = {:PwdExpireTime => attr[:PwdExpireTime].strftime("%A, %d %b %Y %l:%M %p")}
+  elsif attr.keys.include?(:accountExpiresAt) && (-5..1).include?((Time.now - attr[:accountExpiresAt])/86400)
+    accounts_expiring[attr[:mail]] = {:accountExpiresAt => attr[:accountExpiresAt].strftime("%A, %d %b %Y %l:%M %p"), :manager => attr[:manager]}
   else
     puts "Will not notify #{user}, password expires in greater than 5 days: #{attr[:PwdExpireTime]}"
   end
 end
 
-## Slack
-# Initialize Slack Connection
-if !users_to_notify.empty?
-  Slack.configure do |config|
-    config.token = ENV['SLACK_API_TOKEN']
+# Map manager email address to expiring contractor
+managers = {}
+accounts_expiring.each { |m, v| managers[v[:manager].split(",").first.split("=").last] = users[v[:manager].split(",").first.split("=").last][:mail]  }
+managers.each do |manager, email|
+  accounts_expiring.each do |_ , contractor|
+    if contractor[:manager].include?(manager)
+      contractor[:manager_email] = email
+    end
   end
-  client = Slack::Web::Client.new
+end
+
+## Slack
+#Initialize Slack Connection
+Slack.configure do |config|
+  config.token = ENV['SLACK_API_TOKEN']
+end
+client = Slack::Web::Client.new
+slack_users = client.users_list.to_hash
+
+# Notify users of expiring passwords
+if !users_to_notify.empty?
   # Find Slack usernames and add to hash
-  slack_users = client.users_list.to_hash
   slack_users["members"].each do |user|
     if users_to_notify.include?(user["profile"]["email"])
       users_to_notify[user["profile"]["email"]][:slack_name] = user["name"]
@@ -78,3 +107,21 @@ if !users_to_notify.empty?
     end
   end
 end
+
+# Notify managers of expiring contractor accounts
+if !accounts_expiring.empty?
+  accounts_expiring.each do |email, data|
+    slack_manager_data = slack_users["members"].select{ |k, v| k["profile"]["email"] == data[:manager_email]}
+    manager_slack_name = slack_manager_data.first["name"]
+    client.chat_postMessage(channel: "@#{manager_slack_name}", text: "Contractor's account with email #{email} is expiring in less than 5 days at: #{data[:accountExpiresAt]} ", as_user: true)
+  end
+end
+
+
+
+
+
+
+
+
+
